22688ac7cbfc76781aa545db748792fa03a02582
tuannguyen1302 <tuanhienkun13102002@gmail.com>
2023-10-16 01:02:19 +0700

    dangkidangnhap

--- App_shop/App.js
+++ App_shop/App.js
@@ -3,8 +3,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GetStart from "./screens/ClassStart/GetStart";
 import GetStart2 from "./screens/ClassStart/GetStart2";
-import HomeScreen from "./screens/MainScreen";
-
+import Login1 from "./screens/ClassLogin/Login1"
+import LoginWithPassword from "./screens/ClassLogin/LoginWithPassword";
+import SignUp from './screens/ClassLogin/SignUp'
 const StackDemo = createNativeStackNavigator();
 
 const App = () => {
@@ -13,7 +14,9 @@ const App = () => {
       <StackDemo.Navigator screenOptions={{ headerShown: false }}>
         <StackDemo.Screen name="GetStart" component={GetStart} />
         <StackDemo.Screen name="GetStart2" component={GetStart2} />
-        <StackDemo.Screen name="HomeScreen" component={HomeScreen} />
+        <StackDemo.Screen name="Login1" component={Login1} />
+        <StackDemo.Screen name="LoginWithPassword" component={LoginWithPassword} />
+        <StackDemo.Screen name="SignUp" component={SignUp} />
         {/* viết tiếp các màn hình khác vào đây */}
       </StackDemo.Navigator>
     </NavigationContainer>
