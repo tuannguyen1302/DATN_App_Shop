ab6d0d3f47ba29eb3e531b73af7edbb8ce023096
tuannguyen1302 <tuanhienkun13102002@gmail.com>
2023-10-15 08:28:53 +0700

    Your commit message here

--- App_shop/App.js
+++ App_shop/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>ok rồi đó </Text>
+      //me mé mém
       <StatusBar style="auto" />
     </View>
   );
